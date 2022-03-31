0205d74265af0e049e894f735bee7973c15e7777
chryes220 <christinehutabarat12@gmail.com>
2022-04-01 03:01:07 +0700

    add movement functions

--- solver.py
+++ solver.py
@@ -7,6 +7,7 @@ class Solver :
     def __init__(self, puzzle) :
         self.__puzzle = Puzzle()
         self.__puzzle.copy(puzzle)
+        self.__node_count = 1 # jumlah simpul yang dibangkitkan
 
     def kurang(self, no_ubin) :
         loc = self.locate(no_ubin)
@@ -49,3 +50,40 @@ class Solver :
             print("Puzzle dicari dengan metode branch and bound...")
         else :
             print("Puzzle tidak dapat diselesaikan")
+
+    def up(self) :
+        # loc_empty = self.__puzzle.locate(16)
+        i = self.__puzzle.locIdx(16)[0]
+        j = self.__puzzle.locIdx(16)[1]
+        if (i != 0) :
+            # ubin kosong tidak berada di baris dengan indeks 0, tukar dengan ubin atas
+            above = self.__puzzle.getByIdx(i-1, j)
+            self.__puzzle.setByIdx(i,j, above)
+            self.__puzzle.setByIdx(i-1, j, 16)
+
+    def down(self) :
+        i = self.__puzzle.locIdx(16)[0]
+        j = self.__puzzle.locIdx(16)[1]
+        if (i != 3) :
+            # ubin kosong tidak berada di baris dengan indeks 3, tukar dengan ubin bawah
+            below = self.__puzzle.getByIdx(i+1, j)
+            self.__puzzle.setByIdx(i,j, below)
+            self.__puzzle.setByIdx(i+1, j, 16)
+    
+    def left(self) :
+        i = self.__puzzle.locIdx(16)[0]
+        j = self.__puzzle.locIdx(16)[1]
+        if (j != 0) :
+            # ubin kosong tidak berada di kolom dengan indeks 0, tukar dengan ubin kiri
+            left = self.__puzzle.getByIdx(i, j-1)
+            self.__puzzle.setByIdx(i,j, left)
+            self.__puzzle.setByIdx(i, j-1, 16)
+
+    def right(self) :
+        i = self.__puzzle.locIdx(16)[0]
+        j = self.__puzzle.locIdx(16)[1]
+        if (j != 3) :
+            # ubin kosong tidak berada di kolom dengan indeks 3, tukar dengan ubin kanan
+            right = self.__puzzle.getByIdx(i, j+1)
+            self.__puzzle.setByIdx(i,j, right)
+            self.__puzzle.setByIdx(i, j+1, 16)
