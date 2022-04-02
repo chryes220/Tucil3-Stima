7d74fc97d5bc1f1a034a637b1fe3c55fa85d54db
chryes220 <christinehutabarat12@gmail.com>
2022-04-02 15:09:39 +0700

    restructuring

--- src/main.py
+++ src/main.py
@@ -5,10 +5,11 @@ from solver import Solver
 import random
 
 if __name__ == "__main__" :
+    path = "../test/"
     #name = input("Masukkan nama file : ")
     name = "tc_solvable_3.txt"
     u = Puzzle()
-    u.from_file(name)
+    u.from_file(path + name)
     u.displayPuzzle()
     print()
 
