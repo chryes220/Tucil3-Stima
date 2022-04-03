d795c45960e339b5a499c535251b03e97de9f394
chryes220 <christinehutabarat12@gmail.com>
2022-04-03 23:39:08 +0700

    add loop and quit option in main program

--- src/main.py
+++ src/main.py
@@ -5,18 +5,29 @@ from solver import Solver
 import random
 
 if __name__ == "__main__" :
-    path = "test/"
-    u = Puzzle()
+    quit = False
+    while (not quit) :
+        path = "test/"
+        u = Puzzle()
 
-    choice = input("Use randomized puzzle? [y/n] ")
-    if (choice == 'n') :
-        name = input("Input file name : ")
-        u.from_file(path + name)
-    print()
-    
-    print("Initial state")
-    u.displayPuzzle()
-    print()
+        print("Type 'quit' to quit program anytime")
+        choice = input("Use randomized puzzle? [y/n] ")
+        if (choice == 'quit') :
+            quit = True
+        else :
+            if (choice == 'n') :
+                name = input("Input file name : ")
+                if (name == 'quit') :
+                    quit = True
+                else :
+                    u.from_file(path + name)
+            
+            if (not quit) :
+                print()
+                
+                print("Initial state")
+                u.displayPuzzle()
+                print()
 
-    s = Solver(u)
-    s.solve()
+                s = Solver(u)
+                s.solve()
